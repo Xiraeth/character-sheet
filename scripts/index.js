6a4fc4174be77632f7db7af5d03acdaf15797d28
Nick Iliadis <77112995+Xiraeth@users.noreply.github.com>
2023-11-27 12:29:42 +0200

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,46 @@
 'use strict';
 
+const stats = document.querySelector(".stats");
 const barsIcon = document.querySelector(".menuIcon i");
 const xmarkIcon = document.querySelector(".leftNavbar i");
 const menu = document.querySelector(".leftNavbar");
-const showMoreButton = document.querySelector(".fa-circle-chevron-down");
 
+let hitpoints = document.querySelector("#HP div span");/*
+let = document.querySelector("");
+let = document.querySelector("");
+let = document.querySelector("");
+let = document.querySelector("");
+let = document.querySelector("");*/
+
+
+stats.addEventListener("input", (e) => {
+  getModifier(e.target);
+});
 barsIcon.addEventListener("click", showMenu);
 xmarkIcon.addEventListener("click", hideMenu);
-showMoreButton.addEventListener("click", toggleFullTable);
+hitpoints.addEventListener("input", () => {
+	localStorage.setItem("HP", hitpoints.textContent);
+	console.log(localStorage.HP);
+})
+
+function getModifier(abilityScoreElement) {
+	let modifierSpan =
+    abilityScoreElement.parentElement.querySelector(".modifier");
+		
+  let content = abilityScoreElement.textContent;
+  let numericContent = content.replace(/\D/g, "");
+  abilityScoreElement.textContent = numericContent;
+		
+	if (abilityScoreElement.textContent == "") {
+		modifierSpan.textContent = "(...)";
+		return;
+	}
+	
+  let abilityScore = parseInt(numericContent);
+  let modifier = Math.floor((abilityScore - 10) / 2);
+
+  modifierSpan.textContent = `(${modifier >= 0 ? "+" : ""}${modifier})`;
+}
 
 function showMenu() {
 	menu.style.transform = 'translateX(0)';
@@ -17,18 +50,6 @@ function hideMenu() {
 	menu.style.transform = 'translateX(-100px)';
 }
 
-function toggleFullTable(){
-	const allHiddenRows = document.querySelectorAll(".initiallyHidden");
-	
-	if (showMoreButton.classList.contains('fa-circle-chevron-up')) {
-		allHiddenRows.forEach(row => {
-			row.classList.add('hidden');
-			showMoreButton.className = "fa-solid fa-circle-chevron-down";
-		});
-	} else {
-		allHiddenRows.forEach(row => {
-			row.classList.remove('hidden');
-		});
-		showMoreButton.className = "fa-solid fa-circle-chevron-up";
-	}
+function loadPage() {
+	hipoints = localStorage.hitpoints;
 }
