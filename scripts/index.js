dfe9846e618c28de3b1852ebcac76910f394721b
Xiraeth <77112995+Xiraeth@users.noreply.github.com>
2023-11-27 21:13:37 +0200

    Minor width changes
    Change width to min-width at all 3 hitpoint values

--- scripts/index.js
+++ scripts/index.js
@@ -1,41 +1,40 @@
-'use strict';
+"use strict";
 
 const stats = document.querySelector(".stats");
 const barsIcon = document.querySelector(".menuIcon i");
 const xmarkIcon = document.querySelector(".leftNavbar i");
 const menu = document.querySelector(".leftNavbar");
 
-let hitpoints = document.querySelector("#HP div span");/*
+let currentHP = document.querySelector("#HP div span"); /*
 let = document.querySelector("");
 let = document.querySelector("");
 let = document.querySelector("");
 let = document.querySelector("");
 let = document.querySelector("");*/
 
-
 stats.addEventListener("input", (e) => {
   getModifier(e.target);
 });
 barsIcon.addEventListener("click", showMenu);
 xmarkIcon.addEventListener("click", hideMenu);
-hitpoints.addEventListener("input", () => {
-	localStorage.setItem("HP", hitpoints.textContent);
-	console.log(localStorage.HP);
-})
+currentHP.addEventListener("input", () => {
+  localStorage.setItem("HP", currentHP.textContent);
+  console.log(localStorage.getItem("HP"));
+});
 
 function getModifier(abilityScoreElement) {
-	let modifierSpan =
+  let modifierSpan =
     abilityScoreElement.parentElement.querySelector(".modifier");
-		
+
   let content = abilityScoreElement.textContent;
   let numericContent = content.replace(/\D/g, "");
   abilityScoreElement.textContent = numericContent;
-		
-	if (abilityScoreElement.textContent == "") {
-		modifierSpan.textContent = "(...)";
-		return;
-	}
-	
+
+  if (abilityScoreElement.textContent == "") {
+    modifierSpan.textContent = "(...)";
+    return;
+  }
+
   let abilityScore = parseInt(numericContent);
   let modifier = Math.floor((abilityScore - 10) / 2);
 
@@ -43,13 +42,13 @@ function getModifier(abilityScoreElement) {
 }
 
 function showMenu() {
-	menu.style.transform = 'translateX(0)';
+  menu.style.transform = "translateX(0)";
 }
 
 function hideMenu() {
-	menu.style.transform = 'translateX(-100px)';
+  menu.style.transform = "translateX(-100px)";
 }
 
 function loadPage() {
-	hipoints = localStorage.hitpoints;
+  hipoints = localStorage.hitpoints;
 }
